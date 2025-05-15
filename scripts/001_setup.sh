aefd570e9e7472fc10ff63488b606f8d8933c1e4
meirelesgc <geu_costa@outlook.com>
2025-05-14 23:17:14 -0300

    dev: Scripts de setup

---
+++ scripts/001_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/ovictorhugo/admin-module-simcc.git simcc-front
+git clone -b new-simcc https://github.com/meirelesgc/simcc-back.git
+git clone https://github.com/meirelesgc/simcc-back.git simcc-back-old
+git clone https://github.com/meirelesgc/simcc-admin.git
