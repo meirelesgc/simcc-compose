cd1b13f37562d3d0e9c90643f08430c17f61cb05
meirelesgc <geu_costa@outlook.com>
2025-05-15 21:09:54 -0300

    feat: Notas

--- setup.sh
+++ setup.sh
@@ -7,14 +7,25 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-for num in "$@"; do
-    script_path="$SCRIPT_DIR/$(printf "%03d" "$num")_*.sh"
-    
-    if ls $script_path 1> /dev/null 2>&1; then
-        echo "Executing $script_path..."
-        chmod +x $script_path
-        $script_path
-    else
-        echo "Script $script_path not found!"
-    fi
-done
+if [[ " $* " =~ " 0 " ]]; then
+    echo "Executing all scripts..."
+    for script in "$SCRIPT_DIR"/*.sh; do
+        if [[ -f "$script" ]]; then
+            echo "Executing $script..."
+            chmod +x "$script"
+            "$script"
+        fi
+    done
+else
+    for num in "$@"; do
+        script_path="$SCRIPT_DIR/$(printf "%03d" "$num")_*.sh"
+        
+        if ls $script_path 1> /dev/null 2>&1; then
+            echo "Executing $script_path..."
+            chmod +x $script_path
+            $script_path
+        else
+            echo "Script $script_path not found!"
+        fi
+    done
+fi
